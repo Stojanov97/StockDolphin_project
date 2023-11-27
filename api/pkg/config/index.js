e1fee2994ca9ca9075bfcad3bd8a7fe95327082f
Stojanov97 <stojanov091@gmail.com>
2023-11-27 07:07:29 +0100

    config & db pkgs done

--- api/pkg/config/index.js
+++ api/pkg/config/index.js
@@ -1,1 +1,9 @@
+require("dotenv").config();
 
+let get = (section) => {
+  return process.env[section] ? process.env[section] : "Non existing variable";
+};
+
+module.exports = {
+  get,
+};
--- api/pkg/db/index.js
+++ api/pkg/db/index.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
+const config = require("../config").get();
 
+const username = config("MONGO_USERNAME");
+const password = config("MONGO_PASSWORD");
+const cluster = config("MONGO_CLUSTER");
+const database = config("MONGO_DATABASE");
+
+const uri = `mongodb+srv://${username}:${password}@${cluster}/${database}?retryWrites=true&w=majority`;
+
+(async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("Mongo DB connected");
+  } catch (err) {
+    throw new Error(err);
+  }
+})();
